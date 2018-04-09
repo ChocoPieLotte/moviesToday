8f12ade6c2a1288dd45473851bb576550cbba4be
Andrew Torgonsky <atorgonsky@allmax.team>
2018-04-09 15:07:02 +0700

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+const String movieDB = 'http://api.themoviedb.org/3/movie/upcoming?api_key=4493befd452f5d5eeea5c9d2de1306a8';
+const String imagePrefix = 'http://image.tmdb.org/t/p/w500';
---
+++ lib/requester.dart
@@ -0,0 +1,16 @@
+
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'constants.dart' as constants;
+import 'dart:convert';
+
+Future<List> fetchData() async {
+  http.Response response = await http.get(
+      constants.movieDB,
+      headers: {
+        "Accept": "applciation/json"
+      }
+    );
+  print("request data");
+  return JSON.decode(response.body)["results"];
+}
